04cf65b8e41843930f6d454011da138a6142a3ed
blackanger <blackanger.z@gmail.com>
2017-07-20 16:55:24 +0800

    modify time

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 #[macro_use]
 extern crate expedite;
 
-pub use expedite::datetime::period::Period;
-pub use expedite::datetime::time::Time;
+use expedite::datetime::period::Period;
+use expedite::datetime::time::Time;
 
 fn main() {
     let time = Time::now();
 
     println!("{:?}", time);
-    println!("2.days().from_now() : {:?}", 2.days().from_now());
-    println!("2.weeks().from_now() : {:?}", 2.weeks().from_now());
-    println!("2 months {:?}", 2.months().from_now());
-    println!("2 years {:?}", 2.years().from_now());
+    println!("{:?}", 2.days().from_now());
+    println!("{:?}", 2.weeks().from_now());
+    println!("{:?}", 2.months().from_now());
+    println!("{:?}", 2.years().from_now());
 
     let mut map = hash!{'{' => '}', '[' => ']', '(' => ')'};
     println!("{:?}", map);
